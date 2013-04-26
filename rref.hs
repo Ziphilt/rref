7babd617848c31a445e32a2e5847f4d09d8e7106
Cal Stepanian <ziphilt@gmail.com>
2013-04-25 22:49:59 -0400

    cleanup

--- rref.hs
+++ rref.hs
@@ -1,21 +1,16 @@
-{- rref.hs, by Caleb Stepanian
- - row reduces matrices of arbitrary size, using lists and rationals
- -
- - EXAMPLE USAGE
-*Main> let mat3 = [([2,2,9],[7]),([1,0,-3],[8]),([0,1,5],[-2])] :: AugMatrix
-*Main> doRrefAug mat3
-    1     0     0 |     5
-    0     1     0 |     3
-    0     0     1 |    -1
--}
+-- rref.hs, by Caleb Stepanian
+-- row reduces matrices of arbitrary size, using lists and rationals
+-- MA 2071 D Term Project
+-- April 2013
+
 
 {---- IMPORTS ----} --{{{
-import System.Console.Readline
-import Text.ParserCombinators.Parsec
-import Data.Ratio ((%))
+import System.Console.Readline             -- enables convenient line-editing for input
+import Text.ParserCombinators.Parsec       -- parsing library
+import Data.Ratio ((%))                    -- rational number library
 import Data.List (findIndex, sortBy, (\\))
 import Data.Function (on)
-import GHC.Real -- :% -- TODO: why can't I import just this constructor?
+import GHC.Real -- :%
 --}}}
 
 
@@ -63,7 +58,7 @@ padStr n s
   where len = length s
 
 -- convert a Val into a String for pretty-printing
--- only shows a denominator if it is not one
+-- only shows the denominator if it is not equal to 1
 showVal :: Val -> String
 showVal (x:%1) = show x
 showVal (x:%y) = show x ++ "/" ++ show y
@@ -72,10 +67,6 @@ showVal (x:%y) = show x ++ "/" ++ show y
 -- also pads each Val to five characters for pretty printing
 showRow :: Row -> String
 showRow = unwords . (map ((padStr 5) . showVal))
-
--- convert a Matrix into a String
-showMatrix :: Matrix -> String
-showMatrix = unlines . (map showRow)
 
 -- convert an AugRow into a String
 showAugRow :: AugRow -> String
@@ -92,16 +83,6 @@ showAugMatrix = unlines . (map showAugRow)
 mapAug :: (Row -> Row) -> AugRow -> AugRow
 mapAug f (lr, rr) = (f lr, f rr)
 
--- eliminates the second Row using the leading coefficient in the first Row
-eliminate :: Row -> Row -> Row
-eliminate r1 r2 = zipWith (+) (map (*x) r1) r2
-  where
-    x        = -(r2!!i1)/x1 -- the value to pivot with
-    (i1, x1) = firstNonZeroIndex r1 -- find the first nonzero element of r1, or zero otherwise
-    firstNonZeroIndex r = case findIndex (/=0) r of
-                        Just i  -> (i, r!!i)
-                        Nothing -> (0, 1)
-
 -- eliminates the second AugRow using the leading coefficient in the first AugRow
 eliminateAug :: AugRow -> AugRow -> AugRow
 eliminateAug (lr1, rr1) (lr2, rr2) = (zipWith (+) (map (*x) lr1) lr2, zipWith (+) (map (*x) rr1) rr2)
@@ -112,72 +93,50 @@ eliminateAug (lr1, rr1) (lr2, rr2) = (zipWith (+) (map (*x) lr1) lr2, zipWith (+
                         Just i  -> (i, r!!i)
                         Nothing -> (0, 1)
 
--- scales a Row so that its first nonzero element is one
-leadingOne :: Row -> Row
-leadingOne r = map (*(1 / firstNonZero r)) r
-
 -- scales an AugRow so that its first nonzero element is one
 leadingOneAug :: AugRow -> AugRow
 leadingOneAug ar@(lr, rr) = mapAug (map (*(1 / firstNonZero lr))) ar
 
 -- finds the first nonzero element, or uses 1 if all zeros
--- (helper for leadingOne)
+-- (helper for leadingOneAug)
 firstNonZero :: Row -> Val 
 firstNonZero r = case filter (/=0) r of
                (x:xs) -> x
                _      -> 1
 
--- determines whether the given Matrix is rectangular and non-empty
-sane :: Matrix -> Bool
-sane []     = False
-sane (r:rs) = all (\row -> (length row)==(length r)) rs && length r /= 0
-
 -- determines whether the given AugMatrix is rectangular and non-empty
 saneAug :: AugMatrix -> Bool
 saneAug []             = False
-saneAug ((lr, rr):ars) = all (\(l, r) -> (eqLen l lr) && (eqLen r rr)) ars -- && length r /= 0
+saneAug ((lr, rr):ars) = all (\(l, r) -> (eqLen l lr) && (eqLen r rr)) ars
   where
     eqLen :: Row -> Row -> Bool
     eqLen r1 r2 = (length r1)==(length r2)
-
--- wrapper function for reallyRref, which checks for sanity
-rref :: Matrix -> Matrix
-rref m
-  | sane m    = reallyRref [] m
-  | otherwise = error "rref: input matrix is not rectangular or is empty"
 
 -- wrapper function for reallyAugRref, which checks for sanity,
 -- prunes rows of zeros, and sorts by leading coefficients
 rrefAug :: AugMatrix -> AugMatrix
 rrefAug am
   | saneAug am = sortAugMatrix . pruneZeros $ reallyRrefAug [] am
-  | otherwise  = error "rrefAug: input matrix is not rectangular or is empty"
+  | otherwise  = error "rrefAug: input matrix is not rectangular"
 
 -- sort the rows of an AugMatrix by the column of the leading coefficients
 sortAugMatrix :: AugMatrix -> AugMatrix
 sortAugMatrix = sortBy (compare `on` firstNonZeroIndex)
   where
     firstNonZeroIndex (lr,_) = case findIndex (/=0) lr of
-                        Just i  -> i
-                        -- just choose the length if no nonzero elements,
-                        -- so rows of zero go on the bottom
-                        Nothing -> length lr
+                                 Just i  -> i
+                                 -- just choose the length if no nonzero elements,
+                                 -- so rows of zero go on the bottom
+                                 Nothing -> length lr
 
 -- prunes rows of all zeros
 pruneZeros :: AugMatrix -> AugMatrix
 pruneZeros = filter (notAllZeros)
   where notAllZeros (lr, rr) = not $ all (==0) (lr ++ rr)
 
--- helper function to print what rref does to a Matrix
-doRref :: Matrix -> IO ()
-doRref = putStr . showMatrix . rref
-
--- helper function to print what rrefAug does to an AugMatrix
-doRrefAug :: AugMatrix -> IO ()
-doRrefAug = putStr . showAugMatrix . rrefAug
-
 -- determines if a system has a contradiction
 -- there is one if there is a row with all zeros on the left and any nonzero on the right
+-- assumes the system is already rref'd
 hasContradiction :: AugMatrix -> Bool
 hasContradiction = any (contradiction)
   where contradiction (lr, rr) = all (==0) lr && any (/=0) rr
@@ -191,7 +150,8 @@ hasInfiniteSolutions []           = False -- empty system does not have infinite
 hasInfiniteSolutions m@((lr,_):_) = length lr > length m
 
 -- turns a system into a description of what each variable equals
--- assumes the input system is consistent
+-- assumes the input system is consistent,
+-- but does work on both infinite solutions and unique solutions
 showEquations :: AugMatrix -> String
 showEquations m = unlines (map (showEq) m ++ findArb m)
   where
@@ -201,39 +161,37 @@ showEquations m = unlines (map (showEq) m ++ findArb m)
         leadingCoef (l,_) = case findIndex (/=0) l of
                              Just i  -> i
                              Nothing -> length l
-    showEq (lr, (r1:_))  = "x" ++ show leadingVar ++ " = " ++ showVal r1 ++ (concat $ map (showVar lr) indices)
-      where
-        indices     = [(leadingVar+1)..(length lr)-1]
-        showVar r n = if (r!!n == 0) then "" else " + " ++ (("("++) . (++")") . showVal) (-(r!!n)) ++ "*x"++ show n
-        leadingVar  = case findIndex (/=0) lr of
-                      Just i  -> i
-                      Nothing -> error "showEquations: there isn't supposed to be a zero row here"
-{-
-x0 =    3*x2 +   8
-x1 = -9/2*x2 + 7/2
-x2 arbitrary
 
-|x0|      |   3|   |  8|
-|x1| = x2*|-9/2| + |7/2|
-|x2|      |   1|   |  0|
--}
+-- shows a row from a system of equations as a relationship between variables
+-- helper function for showEquations
+showEq :: AugRow -> String
+showEq (lr, (r1:_)) = "x" ++ show leadingVar ++ " = " ++ showVal r1 ++ (concat $ map (showVar lr) indices)
+  where
+    indices     = [(leadingVar+1)..(length lr)-1]
+    showVar r n = if (r!!n == 0) then "" else " + " ++ (("("++).(++")") . showVal) (-(r!!n)) ++ "*x" ++ show n
+    leadingVar  = case findIndex (/=0) lr of
+                    Just i  -> i
+                    Nothing -> error "showEq: there isn't supposed to be a zero row here"
 
--- bundles all the interpretation functions together
+-- bundles all the interpretation functions together,
+-- and produces the output of the program once the user inputs a matrix
 interpretSystem :: AugMatrix -> String
 interpretSystem m = (showAugMatrix reduced) ++ "\n" ++ 
                     if (hasContradiction reduced) then "no solution!\n"
-                      else (if (hasInfiniteSolutions reduced) then "infinite solutions:\n"
-                        else "unique solution:\n")
-                        ++ showEquations reduced
+                      else ((if (hasInfiniteSolutions reduced) then "infinite solutions:\n"
+                                else "unique solution:\n")
+                            ++ showEquations reduced)
   where
     reduced = rrefAug m
 --}}}
 
 
 {---- PARSING ----} --{{{
+-- parses zero or more spaces
 whitespace :: Parser ()
 whitespace = skipMany (char ' ')
 
+-- parses an integer, with optional negative sign
 number :: Parser Integer
 number = (do char '-'
              ds <- many1 digit
@@ -243,6 +201,8 @@ number = (do char '-'
               return (read ds)
            )
 
+-- parses a Val, with optional slash for fractions
+-- allows the denominator to have a sign too
 val :: Parser Val
 val = do a <- number
          (do char '/'
@@ -252,51 +212,47 @@ val = do a <- number
           (do return (a%1)
            )
 
+-- parses a Row, composed of Vals separated by spaces
 row :: Parser Row
 row = sepEndBy1 val whitespace
 
+-- parses an AugRow, composed of two Rows separated by a vertical bar
 augRow :: Parser AugRow
 augRow = do lr <- row
-            --whitespace
             char '|'
             whitespace
             rr <- row
             return (lr, rr)
 
+-- parses a matrix, composed of one or many AugRows separated by newlines
 matrix :: Parser AugMatrix
 matrix = sepEndBy1 augRow newline
 --}}}
 
 
 {---- DO THE REAL WORK ----} --{{{
+-- main method; does all IO
 main = do
   putStrLn "Input rows, and an empty line when finished."
   putStrLn "Separate values by spaces,"
   putStrLn "and the coefficients from the constants by vertical lines."
-  putStrLn "Ex: 0 1 -2/3 4 5/-7 | 9"
+  putStrLn "Example: 0 1 -2/3 4 5/-7 | 9"
   input <- readUntilEmptyLine
   case parse matrix "" input of
-    Left err  -> print err
-    Right mat -> putStr $ interpretSystem mat
+    Left err  -> print err -- if there was a parsing error, print it
+    Right mat -> putStr $ interpretSystem mat -- otherwise interpret the system
  
+-- reads lines from stdin until an empty line is given
+-- uses readline, giving full line editing power
 readUntilEmptyLine :: IO String
 readUntilEmptyLine = do
   line <- readline "> "
   case line of
-    Nothing    -> return ""
-    Just ""    -> return ""
+    Nothing    -> return "" -- EOF on empty line
+    Just ""    -> return "" -- 
     Just line1 -> (do
                   line2 <- readUntilEmptyLine
                   return (line1 ++ "\n" ++ line2))
-
--- actually perform row reduction on a Matrix.
--- the first argument is the list of completed rows, which accumulates
--- as it calls itself recursively
-reallyRref :: Matrix -> Matrix -> Matrix
-reallyRref done []     = done
-reallyRref done (r:rs) = reallyRref (elim done ++ [leadingOne r]) (elim rs)
-  where
-    elim = map (eliminate (leadingOne r))
 
 -- actually perform row reduction on an AugMatrix.
 -- the first argument is the list of completed rows, which accumulates
